f23085ad6cda8b87a1fc9a62f3407fda9fb589cd
decadevvv <32377758+decadevvv@users.noreply.github.com>
2021-08-05 11:26:59 +0800

    Update workflow.go

--- pkg/core/workflow.go
+++ pkg/core/workflow.go
@@ -1,13 +1,14 @@
 package core
 
 type Workflow struct {
-	Name string `yaml:"name" validate:"required"`
-	Doc string `yaml:"doc" validate:"required"`
+	Name  string `yaml:"name" validate:"required"`
+	Doc   string `yaml:"doc" validate:"required"`
 	Steps []Step `yaml:"steps" validate:"required,dive"`
 }
 
 type Step struct {
 	Name   string            `yaml:"name" validate:"required"`
+	Doc    string            `yaml:"doc" validate:"require"`
 	Action string            `yaml:"action" validate:"required"`
 	Conf   map[string]string `yaml:"conf" validate:"required"`
 	Output string            `yaml:"output" validate:"required"`
